64365552623094de6a1ffe7ac3312536edfef79e
flying-hilichurl <lijia_long@foxmail.com>
2025-05-19 10:22:50 +0800

    update: 场景布局框架
    通过一个共同的模版呈现场景

--- src/main/java/hilichurl/mondstadtadvanture/fxmlcontroller/Spot.java
+++ src/main/java/hilichurl/mondstadtadvanture/fxmlcontroller/Spot.java
@@ -1,4 +1,11 @@
 package hilichurl.mondstadtadvanture.fxmlcontroller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class Spot {
+    @FXML
+    public Label description;
+
+
 }
